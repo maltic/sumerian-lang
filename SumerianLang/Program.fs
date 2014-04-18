80cd7b1304b719d6ee9cedda1b73e518ee367767
maltic <max@maltic.com>
2014-04-18 17:30:24 +0800

    Initial import
    Contains basic tokenizing code

---
+++ SumerianLang/Program.fs
@@ -0,0 +1,6 @@
+﻿
+
+[<EntryPoint>]
+let main argv = 
+    Seq.iter (printfn "%A") ( (fun _ -> Token.tokenize (System.Console.ReadLine())) |> Seq.initInfinite )
+    0 // return an integer exit code
